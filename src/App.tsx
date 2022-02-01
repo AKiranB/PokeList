238bb99174bf999ef023c0d9228cd48882527e58
Kiran Boyle <anthony.kiran.boyle@gmail.com>
2022-02-01 16:51:48 +0100

    once again

--- src/App.tsx
+++ src/App.tsx
@@ -4,20 +4,21 @@ import PokemonList from './pages/PokemonList';
 import {
   BrowserRouter,
   Route,
+  Router,
   Routes,
 } from "react-router-dom";
 
 const App = () => {
   return (
     <>
-      <div className="w-full h-screen bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 ...">
-        <BrowserRouter>
-          <Routes>
-            <Route path="/PokeList/" element={<PokemonList />} />
-            <Route path="/PokeList/:name" element={<PokemonDetails />} />
-          </Routes>
-        </BrowserRouter>
-      </div>
+      {/* <div className="w-full h-screen bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 ..."> */}
+      <BrowserRouter>
+        <Routes>
+          <Route path="/PokeList/" element={<PokemonList />} />
+          <Route path="/PokeList/:name" element={<PokemonDetails />} />
+        </Routes>
+      </BrowserRouter>
+      {/* </div> */}
     </>
   )
 };
